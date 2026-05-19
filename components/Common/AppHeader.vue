1c0e6ca431f8cfcd2fda5db97328fd7b6e64fa54
David W. Martin <david.w.martin@rtx.com>
2026-05-19 10:19:57 -0700

    Reorder menu in AppHeader

--- components/Common/AppHeader.vue
+++ components/Common/AppHeader.vue
@@ -264,8 +264,8 @@ const userLoggedIn = ref<boolean>();
 import pdfUrl from '@/assets/styles/pdfs/NGWPC_NgenCERF_Users_Guide.pdf';
 
 const userItems = ref([
-    { label: 'About', icon: 'pi pi-fw-times', command: () => aboutBox() },
     { label: 'Account', icon: 'pi pi-fw-times', command: () => gotoAccount() },
+    { label: 'About', icon: 'pi pi-fw-times', command: () => aboutBox() },
     { label: 'Notifications', icon: 'pi pi-fw-times', command: () => errorLog() },
     { label: 'Users Guide', icon: 'pi pi-fw-times', command: () => window.open(pdfUrl, '_blank') },
     { label: 'Logout', icon: 'pi pi-fw-times', command: () => logoutUser() }
